746edd77cfd7b7b871608b5d03e2132a794a04bd
gitansh wadhwa <gitanshwadhwa0028@gmail.com>
2021-05-01 22:01:14 +0530

    validations

--- routes/home.js
+++ routes/home.js
@@ -16,13 +16,13 @@ router.route("/twitter-help").get((req, res) => {
 
 router.route("/login").get((req, res) => {
 
-  res.render("login.ejs");
+  res.render("login.ejs", { status: 0 });
 
 });
 
 router.route("/register").get((req, res) => {
 
-  res.render("register-1.ejs");
+  res.render("register1.ejs", { status: 0 });
 
 });
 
@@ -36,8 +36,6 @@ router.route("/donate").get((req, res) => {
 //   res.render("register.ejs");
 
 // });
-
-
 router.post('/register-x', async (req, res) => {
 
   let { fullname, age, email, phone, password, state } = req.body;
@@ -46,19 +44,22 @@ router.post('/register-x', async (req, res) => {
   try {
 
     if (!fullname || !email || !password || !phone) {
-      res.render("/register-1.ejs")
+      res.render("register1.ejs", { status: 1 })
+
       return;
     }
-    else if (password.length <= 5){
-      res.render("/register-1.ejs")
-    } 
-  
+    else if (password.length <= 5) {
+      res.render("register1.ejs", { status: 2 })
+
+    }
+
     else {
       sql = "select * from users where email = '" + email + "'";
       db.query(sql, (err, result) => {
 
         if (result[0]) {
-          res.render("/register-1.ejs")
+          res.render("register1.ejs", { status: 3 })
+
           return;
         }
 
@@ -69,38 +70,61 @@ router.post('/register-x', async (req, res) => {
         sql = "insert into users(fullname, age, email ,phone, password, state) values(?,?,?,?,?,?)";
         db.query(sql, [fullname, age, email, phone, password_hashed, state], (err, result) => {
           if (err) {
-            res.render("/register-1.ejs")
+            res.render("register1.ejs", { status: 4 })
+
             return;
           }
           else {
             req.session.email = email;
-            res.render("dashboard.ejs");
+            res.render("dashboard.ejs", { email: req.session.email });
+
           }
 
         });
 
       });
     }
-
-
-
-
   } catch (error) {
     console.log(error)
-    res.render("/register-1.ejs");
+    res.render("register1.ejs", { status: 5 });
     return;
   }
 
 
 });
 
-router.post('/login-x', (req, res) => {
+// router.post('/login-x', (req, res) => {
 
 
 
+//   let { email, password } = req.body;
+//   console.log(req.body)
+//   // var sql = "select * from users where email = '" + email + "'";
+
+//   var sql = 'SELECT * FROM users WHERE email = ?';
+//   db.query(sql, [email], (error, result) => {
+//     console.log(result)
+//     if (result[0]) {
+//       if (bcrypt.compareSync(password, result[0].password)) {
+
+//         req.session.email = result[0].email;
+//         res.render("dashboard.ejs")
+
+//       } else {
+//         res.render("login.ejs", { status: 1 });
+//       }
+//     }
+//     else
+//       res.render("login.ejs", { status: 2 });
+
+//   });
+
+// });
+
+router.route("/login-x").post((req, res) => {
+  var sess = req.session;
   let { email, password } = req.body;
-  console.log(req.body)
-  // var sql = "select * from users where email = '" + email + "'";
+
 
   var sql = 'SELECT * FROM users WHERE email = ?';
   db.query(sql, [email], (error, result) => {
@@ -120,6 +144,7 @@ router.post('/login-x', (req, res) => {
 
   });
 
+
 });
 
 
@@ -127,4 +152,3 @@ router.post('/login-x', (req, res) => {
 
 
 module.exports = router;
-
